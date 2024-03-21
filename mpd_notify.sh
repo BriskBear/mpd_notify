7f82767512437b1b73c5b249668574dc02426103
deftclaw <eeccher@cppwind.com>
2024-03-21 14:57:07 -0600

    Multiple filename formats

--- mpd_notify.sh
+++ mpd_notify.sh
@@ -28,9 +28,16 @@ function mpd-notify() {
   then
     output=`mpc $action`
     status=`echo "$output"|ag '\['|awk -F'\\\]| |\\\[' '{print $2}'`
-    title=`echo "$output"|head -n1|awk -F' - ' '{print $3,$4}'`
+    alt_artist=`echo $output|awk -F'/| ' '{print $2}'`
+    alt_title=`echo $output|awk -F'/| ' '{print $3}'|sed 's/\.flac//g'`
     artist=`echo "$output"|head -n1|awk -F' - ' '{print $2}'`
-    body=`echo -e "$artist\n$title\n[${status}]"`
+    title=`echo "$output"|head -n1|awk -F' - ' '{print $3,$4}'`
+
+    # Unset artist and title if they expand to empty string
+    [ -z ${artist-} ] && unset artist
+    [ -z ${title-}  ] && unset title
+
+    body=`echo -e "${artist:-$alt_artist}\n${title:-$alt_title}\n[${status}]"`
     result=`dunstify -A "$action,default" -i $icon -r 333 -h string:fgcolor:"#ffffcf" -h string:bgcolor:"#1d1f21" 'MPD: ' "$body"`
   else
     msg='mpd is in a state other than: [paused, playing]'
